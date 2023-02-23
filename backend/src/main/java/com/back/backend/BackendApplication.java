60dd02f61aa02eee0d34a3bbc0ab3a722a69f625
unknown <1315498906@qq.com>
2023-02-23 14:39:50 +0800

    后端初步搭建

--- backend/src/main/java/com/back/backend/BackendApplication.java
+++ backend/src/main/java/com/back/backend/BackendApplication.java
@@ -1,11 +1,12 @@
 package com.back.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@MapperScan("com.back.Mapper")
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
---
+++ backend/src/main/java/com/back/pojo/Drugs.java
@@ -0,0 +1,25 @@
+package com.back.pojo;
+
+import java.util.Date;
+
+public class Drugs {
+    private Integer drugsid;
+
+    private String name;
+
+    private Double price;
+
+    private Double purchaseprice;
+
+    private Long num;
+
+    private Date introducedate;
+
+    private Date productdate;
+
+    private Date qualityperiod;
+
+    private String supplyunit;
+
+    private String productunit;
+}
